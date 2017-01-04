abfdc02c2e001e18a5bfd86d9e7c6e63dfb66b18
matrixfox <matrixfox@gmail.com>
2017-01-04 08:18:08 -0800

    Pagination

--- nav-below.php
+++ nav-below.php
@@ -1,6 +1,20 @@
 <?php global $wp_query; if ( $wp_query->max_num_pages > 1 ) { ?>
-<nav id="nav-below" class="navigation" role="navigation">
-<div class="nav-previous"><?php next_posts_link(sprintf( __( '%s older', 'bootslate' ), '<span class="meta-nav">&larr;</span>' ) ) ?></div>
-<div class="nav-next"><?php previous_posts_link(sprintf( __( 'newer %s', 'bootslate' ), '<span class="meta-nav">&rarr;</span>' ) ) ?></div>
+<nav id="nav-below" class="navigation" role="navigation" aria-label="Page navigation">
+<ul class="pagination">
+<li class="page-item">
+<?php
+$big = 999999999; // need an unlikely integer
+echo paginate_links( array(
+'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+'format' => '?paged=%#%',
+'current' => max( 1, get_query_var('paged') ),
+'total' => $wp_query->max_num_pages,
+'show_all'     => True,
+'prev_text'    => __('&laquo;'),
+'next_text'    => __('&raquo;')
+) );
+?>
+</li>
+</ul>
 </nav>
 <?php } ?>
