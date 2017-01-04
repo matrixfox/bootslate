b2f5da4d49a44fd308684a44c973fd77b9c13fbd
matrixfox <matrixfox@gmail.com>
2017-01-04 09:19:14 -0800

    Pager Nav

--- nav-below-single.php
+++ nav-below-single.php
@@ -1,4 +1,6 @@
-<nav id="nav-below" class="navigation" role="navigation">
-<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">&larr;</span> %title' ); ?></div>
-<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">&rarr;</span>' ); ?></div>
+<nav id="nav-below" class="navigation">
+  <ul class="pager">
+    <li><?php previous_post_link( '%link', '<span class="meta-nav">&larr;</span> %title' ) ?></li>
+    <li><?php next_post_link( '%link', '%title <span class="meta-nav">&rarr;</span>' ) ?></li>
+  </ul>
 </nav>
