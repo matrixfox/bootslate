4e191a3283955e3032767a2f3652a71a872463dc
matrixfox <matrixfox@gmail.com>
2017-01-04 09:33:40 -0800

    Remove Spacing

--- nav-below-single.php
+++ nav-below-single.php
@@ -1,6 +1,6 @@
 <nav id="nav-below" class="navigation">
-  <ul class="pager">
-    <li><?php previous_post_link( '%link', '<span class="meta-nav">&larr;</span> %title' ) ?></li>
-    <li><?php next_post_link( '%link', '%title <span class="meta-nav">&rarr;</span>' ) ?></li>
-  </ul>
+<ul class="pager">
+<li><?php previous_post_link( '%link', '<span class="meta-nav">&larr;</span> %title' ) ?></li>
+<li><?php next_post_link( '%link', '%title <span class="meta-nav">&rarr;</span>' ) ?></li>
+</ul>
 </nav>
